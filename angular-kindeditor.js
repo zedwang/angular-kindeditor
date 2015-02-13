f109357e336ffcc4e6974aa0f539c8b4348bc73e
Oliver Wang <sex.xiaoyee@gmail.com>
2015-02-13 22:16:16 +0800

    remove KindEditor.ready()

--- angular-kindeditor.js
+++ angular-kindeditor.js
@@ -4,52 +4,53 @@
  * License: MIT
  * Created by Zed on 19-11-2014.
  */
-(function(window, angular, undefined) {'use strict';
+(function (window, angular) {
+    'use strict';
 
-    /**
-     * @ngdoc overview
-     * @name ngKeditor
-     * @description
-     */
+    angular
+        .module('ngKeditor', [])
+        .directive('keditor', function () {
 
+            var linkFn = function (scope, elm, attr, ctrl) {
 
-    angular.module('ngKeditor', ['ng'])
-        .directive('keditor',function(){
-            return {
-                require : 'ngModel',
-                scope : {
-                    config : '=config'
-                },
-                link : function (scope,elm,attr,ctrl) {
-                    if(typeof KindEditor === "undefined") {
-                        console.error('Please import the local resources of kindeditor!(无法获取到kindeditor资源包！)');
-                        return;
-                    }
-                    var _config = {
-                        uploadJson: '/hummingbird/attachment/uploadimg',
-                        autoHeightMode: false,
-                        afterCreate: function () {
-                            this.loadPlugin('autoheight');
-                        },
-                        width: '100%'
-                    };
-                    var editorId = elm[0];
-                    var editorConfig = scope.config || _config;
-                    editorConfig.afterChange = function () {
-                        if (!scope.$$phase) {
-                            ctrl.$setViewValue(this.html());
-                            scope.$apply();
-                        }
-                    }
-                    KindEditor.ready(function(K){
-                         K.create(editorId,editorConfig);
-                    });
-                    ctrl.$parsers.push(function (viewValue) {
-                        ctrl.$setValidity('keditor', viewValue);
-                        return viewValue;
-                    });
+                if (typeof KindEditor === 'undefined') {
+                    console.error('Please import the local resources of kindeditor!');
+                    return;
                 }
-            }
+
+                var _config = {
+                    width: '100%',
+                    autoHeightMode: false,
+                    afterCreate: function () {
+                        this.loadPlugin('autoheight');
+                    }
+                };
+
+                var editorId = elm[0],
+                    editorConfig = scope.config || _config;
+
+                editorConfig.afterChange = function () {
+                    if (!scope.$$phase) {
+                        ctrl.$setViewValue(this.html());
+                        // exception happens here when angular is 1.2.28
+                        // scope.$apply();
+                    }
+                };
+
+                if (KindEditor) {
+                    KindEditor.create(editorId, editorConfig);
+                }
+
+                ctrl.$parsers.push(function (viewValue) {
+                    ctrl.$setValidity('keditor', viewValue);
+                    return viewValue;
+                });
+            };
+
+            return {
+                require: 'ngModel',
+                scope: { config: '=config' },
+                link: linkFn
+            };
         });
 })(window, window.angular);
-
